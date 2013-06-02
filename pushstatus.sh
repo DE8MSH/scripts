533625df817be45503dbca1caa737b5ca0638eb5
SWL <de8msh@darc.de>
2013-06-02 17:19:02 +0200

    1st script upload

--- pushstatus.sh
+++ pushstatus.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-pachube_api_key="YauKtjXhrGw7XMzvVfL54f9ZlIeSAKxLaDFNN0hYV0JaQT0g"
-pachube_feedt="84554"
+pachube_api_key="key"
+pachube_feedt="feedid"
         m=$(cat /proc/meminfo  | grep "MemFree:" | cut -d":" -f 2 | sed 's/ //g' | sed 's/kB//g')
         u=$(uptime | cut -d" " -f 5 | sed 's/,//g' | sed 's/:/./g')
 	curl --request PUT --header "X-PachubeApiKey: "$pachube_api_key --data ""$m","$u"" "http://www.pachube.com/api/"$pachube_feedt".csv"
